99443a5055a2d715726c7b0cb811854f064dc11f
Asifa Iqbal <asifaiqbal99@gmail.com>
2024-12-17 14:02:05 +0500

    Browsers(chrome/firefox) can be selected

--- src/main/java/utils/WebDriverLoader.java
+++ src/main/java/utils/WebDriverLoader.java
@@ -2,9 +2,20 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Map;
+
+//Singleton Design Pattern for Webdriver
 public class WebDriverLoader {
     private static WebDriver driver;
+    private static String browsers;
+
+    static {
+
+        Map<String, Object> config = ConfigLoader.getConfig();
+        browsers = (String) config.getOrDefault("browser", "chrome");
+    }
 
     // Private constructor to prevent instantiation
     private WebDriverLoader() {
@@ -13,7 +24,17 @@ public class WebDriverLoader {
     // Get the WebDriver instance
     public static WebDriver getDriver() {
         if (driver == null) {
-            driver = new ChromeDriver();
+            switch (browsers) {
+                case "firefox":
+                    System.setProperty("selenium-firefox-driver", "C:\\Users\\pc Planet\\Downloads");
+                    driver = new FirefoxDriver();
+                    break;
+                case "chrome":
+                default:
+                    System.setProperty("selenium-chrome-driver", "C:\\Program Files\\Google\\Chrome\\Application");
+                    driver = new ChromeDriver();
+                    break;
+            }
             driver.manage().deleteAllCookies();
             driver.manage().window().maximize();
         }
