ce06c69004e0831a3fd5f198b3c6cbc88831ddcd
Asifa Iqbal <asifaiqbal99@gmail.com>
2024-12-18 23:36:41 +0500

    added option to specify the browser from cmd

--- src/main/java/utils/WebDriverLoader.java
+++ src/main/java/utils/WebDriverLoader.java
@@ -2,9 +2,16 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Map;
+import java.util.Objects;
 
 public class WebDriverLoader {
     private static WebDriver driver;
+    private static String browser = System.getProperty("browser");
+    static Map<String, Object> config = ConfigLoader.getConfig();
+
 
     // Private constructor to prevent instantiation
     private WebDriverLoader() {
@@ -12,10 +19,20 @@ public class WebDriverLoader {
 
     // Get the WebDriver instance
     public static WebDriver getDriver() {
+        if (browser == null) {
+            System.out.println("No browser specified in the command, getting browser from configuration file.");
+            browser = (String) config.get("browser");
+            System.out.println("Browser Selected: " + browser);
+        }
+
         if (driver == null) {
-            driver = new ChromeDriver();
-            driver.manage().deleteAllCookies();
-            driver.manage().window().maximize();
+            if (Objects.equals(browser, "chrome")) {
+                driver = new ChromeDriver();
+                driver.manage().window().maximize();
+            } else if (Objects.equals(browser, "firefox")) {
+                driver = new FirefoxDriver();
+                driver.manage().window().maximize();
+            }
         }
         return driver;
     }
