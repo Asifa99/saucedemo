890343ccad54b410e07febbddc2ae4a1da840948
Asifa Iqbal <asifaiqbal99@gmail.com>
2024-12-18 23:55:45 +0500

    Updated sorting test as single method

--- src/main/java/Pages/Sorting.java
+++ src/main/java/Pages/Sorting.java
@@ -9,6 +9,7 @@ import utils.WebDriverLoader;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class Sorting {
     public static String sort_dropdown_class = "product_sort_container";
@@ -26,108 +27,104 @@ public class Sorting {
     public Sorting() {
     }
 
-    public void sort_by_a_to_z() {
-        WebElement sortDropdown = driver.findElement(By.className(sort_dropdown_class));
-        //Select class is specifically designed for handling dropdown elements
-        Select sortSelect = new Select(sortDropdown);
-        sortSelect.selectByValue(sort_by_a_to_z_value);
+    public void sort_filter(String sort_by) {
+        if (Objects.equals(sort_by, "a_to_z")) {
+            WebElement sortDropdown = driver.findElement(By.className(sort_dropdown_class));
+            //Select class is specifically designed for handling dropdown elements
+            Select sortSelect = new Select(sortDropdown);
+            sortSelect.selectByValue(sort_by_a_to_z_value);
+        } else if (Objects.equals(sort_by, "z_to_a")) {
+            WebElement sortDropdown = driver.findElement(By.className(sort_dropdown_class));
+            Select sortSelect = new Select(sortDropdown);
+            sortSelect.selectByValue(sort_by_z_to_a_value);
+        } else if (Objects.equals(sort_by, "high_to_low")) {
+            WebElement sortDropdown = driver.findElement(By.className(sort_dropdown_class));
+            Select sortSelect = new Select(sortDropdown);
+            sortSelect.selectByValue(sort_by_high_to_low);
+        } else if (Objects.equals(sort_by, "low_to_high")) {
+            WebElement sortDropdown = driver.findElement(By.className(sort_dropdown_class));
+            Select sortSelect = new Select(sortDropdown);
+            sortSelect.selectByValue(sort_by_low_to_high);
+        } else {
+            System.out.println("Invalid Parameter, Sort by: " + sort_by);
+        }
+
     }
 
-    public static Boolean verify_a_to_z_sorting() {
+    public static Boolean verify_sorting(String sort_by) {
         //Returns a list of all products
         List<WebElement> inventory_item_names = driver.findElements(By.className("inventory_item_name"));
-        //initialize variable
-        int ascii_value_temp = -1;
-        //iterates through each product item
-        for (WebElement item : inventory_item_names) {
-            //fetch text of each product
-            String name = item.getText();
-            //Gets the first character of the name
-            char first_char = name.charAt(0);
-            //Converts the character to its ASCII value.
-            int asciiValue = (int) first_char;
-            //Checks if the current item's first character ASCII value is less than the previous one
-            if (asciiValue < ascii_value_temp) {
-                return false;
+        List<WebElement> inventory_item_prices = driver.findElements(By.className("inventory_item_price"));
+
+        if (Objects.equals(sort_by, "a_to_z")) {
+            //initialize variable
+            int ascii_value_temp = -1;
+            //iterates through each product item
+            for (WebElement item : inventory_item_names) {
+                //fetch text of each product
+                String name = item.getText();
+                //Gets the first character of the name
+                char first_char = name.charAt(0);
+                //Converts the character to its ASCII value.
+                int asciiValue = (int) first_char;
+                //Checks if the current item's first character ASCII value is less than the previous one
+                if (asciiValue < ascii_value_temp) {
+                    return false;
+                }
+                ascii_value_temp = asciiValue;
             }
-            ascii_value_temp = asciiValue;
-        }
-        return true;
-    }
-
-
-    public void sort_by_z_to_a() {
-        WebElement sortDropdown = driver.findElement(By.className(sort_dropdown_class));
-        Select sortSelect = new Select(sortDropdown);
-        sortSelect.selectByValue(sort_by_z_to_a_value);
-    }
-
-    public static Boolean verify_z_to_a_sorting() {
-        List<WebElement> inventory_item_names = driver.findElements(By.className("inventory_item_name"));
-        //Initializes a temporary variable  with a high value 200
-        int ascii_value_temp = 200;
-        for (WebElement item : inventory_item_names) {
-            String name = item.getText();
-            char first_char = name.charAt(0);
-            int asciiValue = (int) first_char;
-            if (asciiValue > ascii_value_temp) {
-                return false;
+            return true;
+        } else if (Objects.equals(sort_by, "z_to_a")) {
+            //Initializes a temporary variable  with a high value 200
+            int ascii_value_temp = 200;
+            for (WebElement item : inventory_item_names) {
+                String name = item.getText();
+                char first_char = name.charAt(0);
+                int asciiValue = (int) first_char;
+                if (asciiValue > ascii_value_temp) {
+                    return false;
+                }
+                ascii_value_temp = asciiValue;
             }
-            ascii_value_temp = asciiValue;
-        }
-        return true;
-    }
+            return true;
+        } else if (Objects.equals(sort_by, "low_to_high")) {
+            float price_temp = 0;
 
-    //low to high price filter
-    public void sort_by_low_to_high() {
-        WebElement sortDropdown = driver.findElement(By.className(sort_dropdown_class));
-        Select sortSelect = new Select(sortDropdown);
-        sortSelect.selectByValue(sort_by_low_to_high);
-    }
-
-    public static Boolean verify_low_to_high_sorting() {
-        List<WebElement> inventory_item_names = driver.findElements(By.className("inventory_item_price"));
-        float price_temp = 0;
-
-        for (WebElement item : inventory_item_names) {
-            String price_str = item.getText().replace("$", "");
-            //convert string into float
-            float price = Float.parseFloat(price_str);
-            if (price_temp == 0) {
+            for (WebElement item : inventory_item_prices) {
+                String price_str = item.getText().replace("$", "");
+                //convert string into float
+                float price = Float.parseFloat(price_str);
+                if (price_temp == 0) {
+                    price_temp = price;
+                    continue;
+                }
+                if (price < price_temp) {
+                    return false;
+                }
                 price_temp = price;
-                continue;
             }
-            if (price < price_temp) {
-                return false;
-            }
-            price_temp = price;
-        }
-        return true;
-    }
+            return true;
+        } else if (Objects.equals(sort_by, "high_to_low")) {
+            float price_temp = 0;
 
-
-    public void sort_by_high_to_low() {
-        WebElement sortDropdown = driver.findElement(By.className(sort_dropdown_class));
-        Select sortSelect = new Select(sortDropdown);
-        sortSelect.selectByValue(sort_by_high_to_low);
-    }
-
-    public static Boolean verify_high_to_low_sorting() {
-        List<WebElement> inventory_item_names = driver.findElements(By.className("inventory_item_price"));
-        float price_temp = 0;
-
-        for (WebElement item : inventory_item_names) {
-            String price_str = item.getText().replace("$", "");
-            float price = Float.parseFloat(price_str);
-            if (price_temp == 0) {
+            for (WebElement item : inventory_item_prices) {
+                String price_str = item.getText().replace("$", "");
+                float price = Float.parseFloat(price_str);
+                if (price_temp == 0) {
+                    price_temp = price;
+                    continue;
+                }
+                if (price > price_temp) {
+                    return false;
+                }
                 price_temp = price;
-                continue;
             }
-            if (price > price_temp) {
-                return false;
-            }
-            price_temp = price;
+            return true;
+        } else {
+            System.out.println("Invalid Parameter, Sort by: " + sort_by);
+            return false;
         }
-        return true;
     }
+
+
 }
--- src/test/java/pagetesting/SortingTest.java
+++ src/test/java/pagetesting/SortingTest.java
@@ -21,20 +21,20 @@ public class SortingTest {
         loginPage.login(driver);
         inventoryPage.open_inventory();
 
-        sorting.sort_by_a_to_z();
-        Boolean a_to_z_sorting_result = Sorting.verify_a_to_z_sorting();
+        sorting.sort_filter("a_to_z");
+        Boolean a_to_z_sorting_result = Sorting.verify_sorting("a_to_z");
         assertEquals(true, a_to_z_sorting_result, "A to Z Sorting Failed.");
 
-        sorting.sort_by_z_to_a();
-        Boolean z_to_a_sorting_result = Sorting.verify_z_to_a_sorting();
+        sorting.sort_filter("z_to_a");
+        Boolean z_to_a_sorting_result = Sorting.verify_sorting("z_to_a");
         assertEquals(true, z_to_a_sorting_result, "Z to A Sorting Failed.");
 
-        sorting.sort_by_low_to_high();
-        Boolean low_to_high_sorting_result = Sorting.verify_low_to_high_sorting();
+        sorting.sort_filter("high_to_low");
+        Boolean low_to_high_sorting_result = Sorting.verify_sorting("high_to_low");
         assertEquals(true, low_to_high_sorting_result, "Price Low to High Sorting Failed.");
 
-        sorting.sort_by_high_to_low();
-        Boolean high_to_low_sorting_result = Sorting.verify_high_to_low_sorting();
+        sorting.sort_filter("low_to_high");
+        Boolean high_to_low_sorting_result = Sorting.verify_sorting("low_to_high");
         assertEquals(true, high_to_low_sorting_result, "Price High to Low Sorting Failed.");
     }
 }
